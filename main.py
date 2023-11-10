3222200ee6c290c104f606d418aae5efbb079aac
Develku <110651468+Develku@users.noreply.github.com>
2023-11-10 23:33:29 +1100

    Llama 2 local AI Poem Generator

--- main.py
+++ main.py
@@ -1,35 +1,29 @@
-# Import the chat model and dotenv loader.
-# This function will load environment variables from a `.env` file located in the same directory as the script.
-# The `.env` file is typically used to store sensitive information such as API keys securely.
 import streamlit as st
-from langchain.chat_models import ChatOpenAI
+# from dotenv import load_dotenv  # Uncomment for local development
 from langchain.llms import CTransformers
-# from dotenv import load_dotenv # for local development
 import time
 
-# Load environment variables containing API keys and other sensitive information.
-# for local development, you can use a `.env` file to store environment variables.
+# Uncomment the line below for local development to load environment variables from a .env file
 # load_dotenv()
 
-# Initialize Streamlit interface and the chat model.
+# Initialize Streamlit interface.
 st.title('AI Poem Generator')
-# chat_model = ChatOpenAI()
+
+# Instantiate the language model.
 llm = CTransformers(
-    model_name="llama-2-7b-chat.ggmlv3.q8_0.bin"
-    model_type="llma"
+    model="llama-2-7b-chat.ggmlv3.q3_K_L.bin",
+    model_type="llama"
 )
 
-# Create a text input for poem title.
+# Input field for the poem title.
 content = st.text_input('Enter the title of the poem:')
 
 # Button to trigger poem generation.
 if st.button('Generate Poem'):
-    # Starting the spinner to indicate that the poem is being generated.
-    with st.spinner('Wait for it...'):
+    with st.spinner('Generating poem...'):
+        # Simulate a processing delay.
         time.sleep(5)
-
-    # Generate a poem using the provided title and display it.
-    prompt = f"{content}, write a very short funny poem about the subject."
-    result = llm.predict(prompt)
-
-    st.write(result)
+        # Generate a poem using the provided title.
+        prompt = f"{content}, write a very short funny poem about the subject."
+        result = llm.predict(prompt)
+        st.write(result)
